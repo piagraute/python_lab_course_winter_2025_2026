0a7b8413dee49ef7dda1ea2608d8cfc84305d321
Pia Graute <piagraute@eduroam-55-113-109.eduroam.ruhr-uni-bochum.de>
2026-03-10 15:24:59 +0100

    implement seed.py

---
+++ src/toolbox/seed.py
@@ -0,0 +1,13 @@
+import random
+import numpy as np
+import torch
+from src.toolbox.config_loader import config_loader
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)    
